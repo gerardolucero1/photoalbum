409baf7df1aa18bef56a617c4fedf998a2882400
gerardolucero1 <gera_conecta@hotmail.com>
2022-02-28 16:16:08 -0700

    cositas

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,8 +8,17 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+                    <div class="grid grid-cols-12 gap-4">
+                        <div class="col-span-6 border rounded-md p-5">
+                            <p class="font-bold text-lg">Vistas</p>
+                            <p class="font-bold text-3xl">0</p>
+                        </div>
+                        <div class="col-span-6 border rounded-md p-5">
+                            <p class="font-bold text-lg">Descargas</p>
+                            <p class="font-bold text-3xl">0</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
